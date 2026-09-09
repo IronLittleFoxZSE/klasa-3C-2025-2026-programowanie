4356517fef85a9241317df534dec45f586973500
Zse <zse@zse.pl>
2026-09-09 11:48:07 +0200

    koniec lekcji

---
+++ Programowanie/EventVsBindingMauiApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿
+namespace EventVsBindingMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            /*Slider slider = sender as Slider;
+            if (rotationLabel is not null
+                && slider is not null)*/
+            //Slider slider = sender as Slider;
+            if (rotationLabel is not null
+                && sender is Slider slider)
+            {
+                rotationLabel.Rotation = slider.Value;
+                rotationLabel.Text = slider.Value.ToString();
+            }
+        }
+    }
+}
