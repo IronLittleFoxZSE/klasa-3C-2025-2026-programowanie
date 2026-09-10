1c4bbe1a3978d8e0743ddba38426a25d4532d8b9
Zse <zse@zse.pl>
2026-09-10 15:15:42 +0200

    koniec lekcji

--- Programowanie/EventVsBindingMauiApp/MainPage.xaml.cs
+++ Programowanie/EventVsBindingMauiApp/MainPage.xaml.cs
@@ -3,7 +3,6 @@ namespace EventVsBindingMauiApp
 {
     public partial class MainPage : ContentPage
     {
-
         public MainPage()
         {
             InitializeComponent();
@@ -21,6 +20,33 @@ namespace EventVsBindingMauiApp
                 rotationLabel.Rotation = slider.Value;
                 rotationLabel.Text = slider.Value.ToString();
             }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            resultMessageLabel.Text = messageEntry.Text;
+        }
+
+
+        public string Message { get; set; }
+
+        private string returnMessage;
+        public string ReturnMessage
+        {
+            get { return returnMessage; }
+            set 
+            { 
+                returnMessage = value;
+                //OnPropertyChanged(nameof(ReturnMessage));
+                OnPropertyChanged();
+            }
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            ReturnMessage = Message;
+            //OnPropertyChanged("ReturnMessage");
+            //OnPropertyChanged(nameof(ReturnMessage));
         }
     }
 }
